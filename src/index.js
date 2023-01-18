386968241826870c5ce181c5743cbc01d3466f6b
Matt <mmaguire333@gmail.com>
2023-01-18 13:42:51 -0700

    Create a navbar to switch between the home, menu, and staff pages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import loadHomePage from './loadHomePage';
 import loadMenu from './loadMenu';
 import loadStaffPage from './loadStaffPage';
-loadMenu();
+
+loadHomePage();
+
+let homeBtn = document.getElementById('home-button');
+let menuBtn = document.getElementById('menu-button');
+let staffBtn = document.getElementById('staff-button');
+let pageContainer = document.getElementById('content');
+
+homeBtn.addEventListener('click', () => {
+    pageContainer.replaceChildren();
+    loadHomePage();
+});
+
+menuBtn.addEventListener('click', () => {
+    pageContainer.replaceChildren();
+    loadMenu();
+});
+
+staffBtn.addEventListener('click', () => {
+    pageContainer.replaceChildren();
+    loadStaffPage();
+});
